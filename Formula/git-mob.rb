bc2a43d660b3308d89859b81c0a35cd6f0f084a6
Martin Frost <martin@frost.ws>
2021-08-20 20:10:54 +0000

    git-mob: add 0.3.4 bottle.

--- Formula/git-mob.rb
+++ Formula/git-mob.rb
@@ -5,6 +5,11 @@ class GitMob < Formula
   sha256 "2d0c9f51990c69e000310d37754592ad0a240d82b12ce78cd48898a89a477479"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/Frost/homebrew-tap/releases/download/git-mob-0.3.4"
+    sha256 cellar: :any_skip_relocation, catalina: "0df6ae76f594b8f5b67f518782cb4cc72ace4001c6edc3df4bb79ab1f37fe08a"
+  end
+
   def install
     bin.install "git-add-coauthor"
     bin.install "git-edit-coauthor"
