cbd31f66011e99c23d0ffdf50939c380ac5941b1
Martin Frost <frost@ceri.se>
2021-08-21 21:11:09 +0200

    Try to fix git-mob formula

--- Formula/git-mob.rb
+++ Formula/git-mob.rb
@@ -3,12 +3,10 @@ class GitMob < Formula
   homepage "https://github.com/Frost/git-mob"
   license "MIT"
 
-  on_macos do
+  if OS.mac?
     url "https://github.com/Frost/git-mob/releases/download/v0.3.5/git-mob-0.3.5-macos.zip"
     sha256 "884cee1574e4e5fbc841aa955f03a938dcff26f188639956913447aa64042c5b"
-  end
-
-  on_linux do
+  elsif OS.linux?
     url "https://github.com/Frost/git-mob/releases/download/v0.3.5/git-mob-0.3.5-ubuntu.zip"
     sha256 "b234ff8c673697f141b9456b823f33c9a37984c83d07a735e1e9c4c7a5f0d602"
   end
