305df00918378cf878fd714491d07143c374f8bc
Martin Frost <martin@frost.ws>
2021-12-25 20:14:23 +0000

    git-mob: update 0.4.0 bottle.

--- Formula/git-mob.rb
+++ Formula/git-mob.rb
@@ -6,8 +6,8 @@ class GitMob < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/Frost/homebrew-tap/releases/download/git-mob-0.3.5"
-    sha256 cellar: :any_skip_relocation, catalina: "89add6ebee7e0e1ee78ad7bef9ac523c9ae66d58f7658c4c308f912c000314bb"
+    root_url "https://github.com/Frost/homebrew-tap/releases/download/git-mob-0.4.0"
+    sha256 cellar: :any_skip_relocation, catalina: "e54bfb688a6b4014703c0cc3ddc5ecb088d3141b4402dc919e08ac794c95fb12"
   end
 
   def install
